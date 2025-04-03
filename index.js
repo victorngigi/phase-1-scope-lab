95b7725e6f6c7c8981d5848840c13a7bfa55219f
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-04-03 11:47:38 +0300

    final

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = `bob`;
+upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase();    
+}
+setBestCustomer = () => {
+    bestCustomer = `not bob`;
+    return bestCustomer;
+}
+overwriteBestCustomer = () => {
+    bestCustomer = `maybe bob`;
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = `not bob`;
+changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = `bob`;
+    return leastFavoriteCustomer;
+}
